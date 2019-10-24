7de83a888b564ccf81f13f1da60f1e53b6ea4600
Rubyet <rittick.2012@gmail.com>
2019-10-24 10:38:04 +0600

    initial commit

---
+++ controllers/home.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var db = require('./../models/db.js');
+var router = express.Router();
+
+router.get('/', function(req, res){
+
+		var sql = "select * from user";
+		db.getResults(sql, function(results){
+
+			res.render('home/index', {user: results});
+			
+		});
+});
+
+router.get('/test/:name/:id', function(req, res){
+
+	res.send(req.params.id+ "|"+req.params.name)
+})
+
+
+
+module.exports = router;
+
+
